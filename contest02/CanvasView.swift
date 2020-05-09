3440d827dd2021261ea41174a6571c91c98d7673
Elaine <elainexu392@gmail.com>
2020-05-09 10:48:32 -0400

    Made the code length shorter and neater.

--- contest02/CanvasView.swift
+++ contest02/CanvasView.swift
@@ -14,6 +14,16 @@ class CanvasView: UIView {
         contest2()
     }
     func contest2()  {
+        // x: 100,y:300
+        // x: 150, y: 250
+        // x:200, y:200
+        // x:270, y:130
+        // x:310, y:90
+        // x: 250, y:150
+        // x: 230, y:170
+        // x: 210, y: 190
+        // x: 180, y :220
+        // x: 130, y; 270
         let pencil = UIBezierPath()
         pencil.move(to: CGPoint(x: 150, y: 500))
         pencil.addLine(to: CGPoint(x: 550, y: 500))
@@ -25,68 +35,35 @@ class CanvasView: UIView {
         let dot2 = UIBezierPath(arcCenter: CGPoint(x: 450, y: 300), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
         dot2.fill()
         #colorLiteral(red: 0.9297945205, green: 0.6243325418, blue: 0.8457076168, alpha: 1).setStroke()
-        #colorLiteral(red: 0.8078431487, green: 0.02745098062, blue: 0.3333333433, alpha: 1).setFill()
-        let dot20 = UIBezierPath(arcCenter: CGPoint(x: 215, y: 175), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot20.fill()
         
-        let dot19 = UIBezierPath(arcCenter: CGPoint(x: 205, y: 435), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot19.fill()
+        #colorLiteral(red: 0.8955479452, green: 0.2406833024, blue: 0.4920867779, alpha: 1).setFill()
+        let answerPoints: [CGPoint] = [
+            CGPoint(x: 215, y: 175),
+            CGPoint(x: 205, y: 435),
+            CGPoint(x: 290, y: 145),
+            CGPoint(x: 290, y: 455),
+            CGPoint(x: 345, y: 145),
+            CGPoint(x: 345, y: 455),
+            CGPoint(x: 375, y: 145),
+            CGPoint(x: 375, y: 455),
+            CGPoint(x: 405, y: 155),
+            CGPoint(x: 405, y: 445),
+            CGPoint(x: 500, y: 225),
+            CGPoint(x: 500, y: 375),
+            CGPoint(x: 440, y: 170),
+            CGPoint(x: 440, y: 430),
+            CGPoint(x: 325, y: 145),
+            CGPoint(x: 325, y: 455),
+            CGPoint(x: 230, y: 150),
+            CGPoint(x: 230, y: 450),
+            CGPoint(x: 510, y: 220),
+            CGPoint(x: 410, y: 380),
+        ]
         
-        let dot18 = UIBezierPath(arcCenter: CGPoint(x: 290, y: 145), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot18.fill()
-        
-        let dot17 = UIBezierPath(arcCenter: CGPoint(x: 290, y: 455), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot17.fill()
-        
-        let dot16 = UIBezierPath(arcCenter: CGPoint(x: 345, y: 145), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot16.fill()
-        
-        let dot15 = UIBezierPath(arcCenter: CGPoint(x: 345, y: 455), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot15.fill()
-        
-        let dot14 = UIBezierPath(arcCenter: CGPoint(x: 375, y: 145), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot14.fill()
-        
-        let dot13 = UIBezierPath(arcCenter: CGPoint(x: 375, y: 455), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot13.fill()
-        
-        let dot12 = UIBezierPath(arcCenter: CGPoint(x: 405, y: 155), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot12.fill()
-        
-        let dot11 = UIBezierPath(arcCenter: CGPoint(x: 405, y: 445), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot11.fill()
-        
-        let dot10 = UIBezierPath(arcCenter: CGPoint(x: 500, y: 225), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot10.fill()
-        
-        let dot9 = UIBezierPath(arcCenter: CGPoint(x: 500, y: 375), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot9.fill()
-        
-        let dot8 = UIBezierPath(arcCenter: CGPoint(x: 440, y: 170), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot8.fill()
-        
-        let dot7 = UIBezierPath(arcCenter: CGPoint(x: 440, y: 430), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot7.fill()
-        
-        let dot6 = UIBezierPath(arcCenter: CGPoint(x: 325, y: 145), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot6.fill()
-        
-        let dot5 = UIBezierPath(arcCenter: CGPoint(x: 325, y: 455), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot5.fill()
-        
-        let dot4 = UIBezierPath(arcCenter: CGPoint(x: 230, y: 150), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot4.fill()
-        
-        let dot3 = UIBezierPath(arcCenter: CGPoint(x: 230, y: 450), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot3.fill()
-        
-        let dot02 = UIBezierPath(arcCenter: CGPoint(x: 510, y: 220), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot02.fill()
-        
-        let dot01 = UIBezierPath(arcCenter: CGPoint(x: 410, y: 380), radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        dot01.fill()
-       
-
+        for point in answerPoints {
+            let dot0 = UIBezierPath(arcCenter: point, radius: 5, startAngle:  0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
+            dot0.fill()
+        }
     }
     
 }
