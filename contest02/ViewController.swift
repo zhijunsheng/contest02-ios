8942795c901a73c79591cd3db145ac2823a471b3
Bowen Lin <bowenlin@lins-MacBook-Pro.local>
2020-05-09 11:20:49 -0400

    made new project for contest02

--- contest02/ViewController.swift
+++ contest02/ViewController.swift
@@ -2,8 +2,8 @@
 //  ViewController.swift
 //  contest02
 //
-//  Created by ethan on 2020/5/7.
-//  Copyright © 2020 ethan. All rights reserved.
+//  Created by Bowen Lin on 2020-05-09.
+//  Copyright © 2020 Bowen Lin. All rights reserved.
 //
 
 import UIKit
@@ -12,7 +12,12 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
     }
 
 
