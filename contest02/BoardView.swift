08fbe15e66e0afa5fc4889c387e471fe5c99e851
daobao <lucashongyuanzhang@gmail.com>
2020-05-10 09:20:15 +0800

    we have a REAL bowl

--- contest02/BoardView.swift
+++ contest02/BoardView.swift
@@ -35,18 +35,6 @@ class BoardView: UIView {
         rulerpath.addLine(to: CGPoint(x: 728, y: 564))
         rulerpath.stroke()
         
-        let compassPath = UIBezierPath(arcCenter: CGPoint(x: 250, y: 564), radius: 5, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        UIColor.blue.setFill()
-        compassPath.fill()
-        UIColor.blue.setStroke()
-        compassPath.stroke()
-        
-        let compassPath1 = UIBezierPath(arcCenter: CGPoint(x: 478, y: 564), radius: 5, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        UIColor.blue.setFill()
-        compassPath1.fill()
-        UIColor.blue.setStroke()
-        compassPath1.stroke()
-        
         let rulerLengend = UIBezierPath()
         rulerLengend.move(to: CGPoint(x: 0, y: bounds.height - 50))
         rulerLengend.addLine(to: CGPoint(x: bounds.width, y: bounds.height - 50))
@@ -69,26 +57,23 @@ class BoardView: UIView {
             compassPath3.stroke()
         }
         
-        let compassPath2 = UIBezierPath(arcCenter: CGPoint(x: 364, y: 621), radius: 5, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
-        UIColor.blue.setFill()
-        compassPath2.fill()
-        UIColor.blue.setStroke()
-        compassPath2.stroke()
-        
         let answerPointsBox: [CGPoint] = [
-            CGPoint(x: 524.5, y: 517),
-            CGPoint(x: 551, y: 469.5),
-            CGPoint(x: 584, y: 419),
-            CGPoint(x: 610.5, y: 365.5),
-            CGPoint(x: 125.5, y: 365.5),
-            CGPoint(x: 149, y: 419),
-            CGPoint(x: 174, y: 469),
-            CGPoint(x: 210, y: 517),
-            CGPoint(x: 627, y: 313.5),
-            CGPoint(x: 651, y: 268),
-            CGPoint(x: 669, y: 219),
-            CGPoint(x: 685, y: 167),
             CGPoint(x: 702.5, y: 116),
+            CGPoint(x: 685, y: 167),
+            CGPoint(x: 669, y: 219),
+            CGPoint(x: 651, y: 268),
+            CGPoint(x: 627, y: 313.5),
+            CGPoint(x: 610.5, y: 365.5),
+            CGPoint(x: 584, y: 419),
+            CGPoint(x: 551, y: 469.5),
+            CGPoint(x: 524.5, y: 517),
+            CGPoint(x: 478, y: 564),
+            CGPoint(x: 364, y: 621),
+            CGPoint(x: 250, y: 564),
+            CGPoint(x: 210, y: 517),
+            CGPoint(x: 174, y: 469),
+            CGPoint(x: 149, y: 419),
+            CGPoint(x: 125.5, y: 365.5),
             CGPoint(x: 102.5, y: 313.5),
             CGPoint(x: 90.5, y: 268),
             CGPoint(x: 66.5, y: 219),
@@ -100,6 +85,13 @@ class BoardView: UIView {
             let compassPath3 = UIBezierPath(arcCenter: point, radius: 5, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
             compassPath3.fill()
             compassPath3.stroke()
+        }
+        
+        let bowl = UIBezierPath()
+        for i in 0..<answerPointsBox.count - 1 {
+            bowl.move(to: answerPointsBox[i])
+            bowl.addLine(to: answerPointsBox[i + 1])
+            bowl.stroke()
         }
     }
 }
