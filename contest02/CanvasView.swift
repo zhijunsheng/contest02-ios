63a927e64dc3b2ddfb8b8f99b9168b5ce9fdacdb
Hal Wang <halhaoranwang@gmail.com>
2020-05-10 20:09:58 +0800

    we added the CanvasView

---
+++ contest02/CanvasView.swift
@@ -0,0 +1,16 @@
+//
+//  CanvasView.swift
+//  contest02
+//
+//  Created by Halwong on 2020/5/10.
+//  Copyright © 2020 ethan. All rights reserved.
+//
+
+import UIKit
+
+class CanvasView: UIView {
+    override func draw(_ rect: CGRect) {
+        
+    }
+
+}
