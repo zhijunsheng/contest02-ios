0b7f978be3e262b4bbc3008831a74143b9a5d81f
Andy Yan <andy@Andys-MacBook-Air.local>
2020-05-10 16:44:26 -0400

    we fixed the problem when the line wasn't the same as thee arcs in our project

--- contest02/SquareView.swift
+++ contest02/SquareView.swift
@@ -18,11 +18,11 @@ class SquareView: UIView {
         pencil1.move(to: CGPoint(x: 100, y: 175))
         pencil1.addLine(to: CGPoint(x: 100, y: 225))
         
-        pencil1.move(to: CGPoint(x: 600, y: 175))
-        pencil1.addLine(to: CGPoint(x: 600, y: 225))
+        pencil1.move(to: CGPoint(x: 550, y: 175))
+        pencil1.addLine(to: CGPoint(x: 550, y: 225))
         
         pencil1.move(to: CGPoint(x: 100, y: 200))
-        pencil1.addLine(to: CGPoint(x: 600, y: 200))
+        pencil1.addLine(to: CGPoint(x: 550, y: 200))
         
         //2 x's
         pencil1.move(to: CGPoint(x: 100, y: 500))
@@ -44,21 +44,16 @@ class SquareView: UIView {
         #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1).setStroke()
         pencil2.stroke()
         
-        let pencil3 = UIBezierPath(arcCenter: CGPoint(x: 125, y: 525), radius: 175, startAngle: CGFloat.pi * 1.7, endAngle: CGFloat.pi * 1.9, clockwise: true)
+        let pencil3 = UIBezierPath(arcCenter: CGPoint(x: 125, y: 525), radius: 175, startAngle: CGFloat.pi * 0, endAngle: CGFloat.pi * 2, clockwise: true)
         
         #colorLiteral(red: 0.7450980544, green: 0.1568627506, blue: 0.07450980693, alpha: 1).setStroke()
         pencil3.stroke()
         
-        let pencil4 = UIBezierPath(arcCenter: CGPoint(x: 100, y: 200), radius: 200, startAngle: CGFloat.pi * 1.9, endAngle: CGFloat.pi * 0.1, clockwise: true)
+        let pencil6 = UIBezierPath(arcCenter: CGPoint(x: 525, y: 525), radius: 275, startAngle: CGFloat.pi * 0, endAngle: CGFloat.pi * 2, clockwise: false)
         
-        #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1).setStroke()
-        pencil4.stroke()
-        
-        let pencil5 = UIBezierPath(arcCenter: CGPoint(x: 525, y: 525), radius: 200, startAngle: CGFloat.pi * 1.9, endAngle: CGFloat.pi * 0.1, clockwise: true)
-        
-        #colorLiteral(red: 0.1215686277, green: 0.01176470611, blue: 0.4235294163, alpha: 1).setStroke()
-        pencil5.stroke()
-        
+        #colorLiteral(red: 0.7254902124, green: 0.4784313738, blue: 0.09803921729, alpha: 1).setStroke()
+        pencil6.stroke()
+    
     }
     
 
