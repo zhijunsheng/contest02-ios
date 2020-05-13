aeb5546e23e745da287e5634a271fe52d94c19b4
Angie Yan <angieyan@Angies-MacBook-Pro.local>
2020-05-13 17:15:36 -0400

    I added even more points and found some special ones aligned to the two problem points.

--- contest02/BoardView.swift
+++ contest02/BoardView.swift
@@ -29,6 +29,23 @@ class BoardView: UIView {
             CGPoint(x: 355.5, y: 326.5),
             CGPoint(x: 419, y: 339),
             CGPoint(x: 419, y: 464),
+            CGPoint(x: 295, y: 333),
+            CGPoint(x: 294.5, y: 467),
+            CGPoint(x: 257, y: 449.5),
+            CGPoint(x: 269.5, y: 343),
+            CGPoint(x: 270.5, y: 458),
+            CGPoint(x: 443.5, y: 352),
+            CGPoint(x: 443.5, y: 450),
+            CGPoint(x: 318.5, y: 327.5),
+            CGPoint(x: 319, y: 472.5),
+            CGPoint(x: 381, y: 473),
+            CGPoint(x: 350, y: 476),
+            CGPoint(x: 475, y: 400),
+            CGPoint(x: 225, y: 400),
+            CGPoint(x: 233.5, y: 374),
+            CGPoint(x: 234, y: 427),
+            CGPoint(x: 466, y: 374),
+            CGPoint(x: 467, y: 424.5)
         ]
         
         for point in answerPoints {
@@ -37,11 +54,12 @@ class BoardView: UIView {
             point1.fill()
         }
         
-        let arc1 = UIBezierPath(arcCenter: CGPoint(x: 250, y: 400), radius: 80, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let arc1 = UIBezierPath(arcCenter: CGPoint(x: 250, y: 400), radius: 225 - 7, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         arc1.stroke()
         
-        let arc2 = UIBezierPath(arcCenter: CGPoint(x: 450, y: 400), radius: 170, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
+        let arc2 = UIBezierPath(arcCenter: CGPoint(x: 450, y: 400), radius: 25 + 7, startAngle: 0, endAngle: 2 * CGFloat.pi, clockwise: true)
         arc2.stroke()
+    
     }
     
     override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
