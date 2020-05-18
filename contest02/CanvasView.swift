c957b18b7f75c9a7105e64dc2f7a10b17c1de7ce
Grace Huang <gracehuang@Graces-MacBook-Air.local>
2020-05-17 20:23:47 -0500

    automated mark placing

--- contest02/CanvasView.swift
+++ contest02/CanvasView.swift
@@ -14,14 +14,22 @@ class CanvasView: UIView {
     override func draw(_ rect: CGRect) {
         question1()
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let first = touches.first!
+        let fingerLocation = first.location(in: self)
+        print("\(fingerLocation.x), \(fingerLocation.y)")
+    }
+    
+    
     func question1() {
         let pen = UIBezierPath()
         let parallel = UIBezierPath()
         let compass = UIBezierPath()
-        let answer = UIBezierPath()
-        
-        pen.move(to: CGPoint(x: 100, y: 650))
-        pen.addLine(to: CGPoint(x: 600, y: 650))
+        let distance: CGFloat = 30
+
+        pen.move(to: CGPoint(x: 0, y: 650))
+        pen.addLine(to: CGPoint(x: 1000, y: 650))
         
         pen.move(to: CGPoint(x: 320, y: 540))
         pen.addLine(to: CGPoint(x: 340, y: 560))
@@ -33,17 +41,28 @@ class CanvasView: UIView {
         
         #colorLiteral(red: 0.2196078449, green: 0.007843137719, blue: 0.8549019694, alpha: 1).setStroke()
         
-        // parallel line
-        parallel.move(to: CGPoint(x: 175, y: 520))
-        parallel.addLine(to: CGPoint(x: 600, y: 520))
         
-        parallel.lineWidth = 2
-        parallel.stroke()
+        for i in 0..<7 {
+//        let i = 1
         
-        compass.addArc(withCenter: CGPoint(x: 330, y: 550), radius: 145, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
-        compass.stroke()
+            parallel.move(to: CGPoint(x: 0, y: 520 - distance * CGFloat(i)))
+            parallel.addLine(to: CGPoint(x: 1000, y: 520 - distance * CGFloat(i)))
+
+            parallel.lineWidth = 1
+            parallel.stroke()
+
+            compass.addArc(withCenter: CGPoint(x: 330, y: 550), radius: 145 + distance * CGFloat(i), startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: true)
+            compass.stroke()
+
+            let c: CGFloat = 145 + distance * CGFloat(i)
+            let a: CGFloat = distance * CGFloat(i + 1)
+            let b: CGFloat = sqrt(c * c - a * a)
+
+            mark(x: 330 - b, y: 520 - distance * CGFloat(i))
+            mark(x: 330 + b, y: 520 - distance * CGFloat(i))
+        }
         
-        mark(x: 187, y: 520)
+        
     }
     
     func mark(x: CGFloat, y: CGFloat) {
@@ -55,19 +74,10 @@ class CanvasView: UIView {
         pen.addLine(to: CGPoint(x: x - 10, y: y + 10))
         
         #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).setStroke()
+        pen.lineWidth = 3
         pen.stroke()
     }
     
-    func drawCross(x: CGFloat, y: CGFloat) {
-        let ruler = UIBezierPath()
-        ruler.move(to: CGPoint(x: x, y: y - 75))
-        ruler.addLine(to: CGPoint(x: x, y: y + 75))
-        ruler.move(to: CGPoint(x: x - 75, y: y))
-        ruler.addLine(to: CGPoint(x: x + 75, y: y))
-        
-        #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).setStroke()
-        ruler.stroke()
-    }
     
 }
 
