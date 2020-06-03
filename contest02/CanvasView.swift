f1a45dec595d576c164f4e8d70c28faabe9552e8
Lambert Lin <lambertlin@mayingdeMacBook-Pro.local>
2020-06-03 22:22:32 +0800

    We built a very beautiful necklace.

--- contest02/CanvasView.swift
+++ contest02/CanvasView.swift
@@ -9,15 +9,23 @@
 import UIKit
 
 class CanvasView: UIView {
-    var flag = -23
+    var flag = 1
     
     override func draw(_ rect: CGRect) {
         if flag == 1 {
             we2()
         } else if flag == 2 {
-            problem()
+            问题()
         }
+        
     }
+    
+    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+        let first = touches.first!
+        let fingerLocation = first.location(in: self)
+        print("\(fingerLocation.x), \(fingerLocation.y)")
+    }
+    
     func we2() {
             
         let awe1234 = UIBezierPath(arcCenter: CGPoint(x: 364, y: 400), radius: 150, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
@@ -149,6 +157,10 @@ class CanvasView: UIView {
         
         
         let answerPoints: [CGPoint] = [
+            CGPoint(x: 85, y: 50),
+            CGPoint(x: 647, y: 50),
+            CGPoint(x: 70, y: 0),
+            CGPoint(x: 660, y: 0),
             CGPoint(x: 538, y: 300),
             CGPoint(x: 193, y: 300),
             CGPoint(x: 163, y: 250),
@@ -163,47 +175,102 @@ class CanvasView: UIView {
             CGPoint(x: 611, y: 150),
             CGPoint(x: 100, y: 100),
             CGPoint(x: 634, y: 100),
-            CGPoint(x: 85, y: 50),
-            CGPoint(x: 647, y: 50),
-            CGPoint(x: 70, y: 0),
-            CGPoint(x: 660, y: 0),
             CGPoint(x: 364, y: 450),
             CGPoint(x: 310, y: 433),
             CGPoint(x: 425, y: 433),
+            CGPoint(x: 331.5, y: 358.5),
         ]
         for point in answerPoints {
             let ee14 = UIBezierPath(arcCenter: point , radius: 3, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
             #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).setFill()
             ee14.fill()
         }
-    }
-    func problem() {
-        let awe = UIBezierPath(arcCenter: CGPoint(x: 124, y:300), radius: 3, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
-        #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1).setFill()
-        awe.fill()
         
-        let awe1 = UIBezierPath(arcCenter: CGPoint(x: 324, y:300), radius: 3, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
+        let h: CGFloat = 100
+        
+        for i in 0..<80 {
+            let b: CGFloat = 0 + CGFloat(i * 10)
+            let a: CGFloat = (b * b - h * h) / (2 * h)
+            let y: CGFloat = 400 - a
+            UIBezierPath(arcCenter: CGPoint(x: 364 - b , y: y) , radius: 9, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false).fill()
+            UIBezierPath(arcCenter: CGPoint(x: 364 + b , y: y) , radius: 9, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false).fill()
+        }
+    }
+    
+    func 问题() {
         #colorLiteral(red: 0.4392156899, green: 0.01176470611, blue: 0.1921568662, alpha: 1).setFill()
-        awe1.fill()
+        UIBezierPath(arcCenter: CGPoint(x: 124, y:300), radius: 3, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false).fill()
+        
+        UIBezierPath(arcCenter: CGPoint(x: 324, y:300), radius: 3, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false).fill()
         
         let pencil = UIBezierPath()
         pencil.move(to: CGPoint(x: 54, y: 700))
         pencil.addLine(to: CGPoint(x: 394, y: 700))
         pencil.stroke()
         
-        let awe12 = UIBezierPath(arcCenter: CGPoint(x: 124, y:300), radius: 70, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
-        awe12.stroke()
+        #colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1).setStroke()
+        for i in 0..<30 {
+            UIBezierPath(arcCenter: CGPoint(x: 124, y:300), radius: CGFloat(90 + i * 15), startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false).stroke()
+           
+            
+            UIBezierPath(arcCenter: CGPoint(x: 324, y:300), radius: CGFloat(250 - i * 15) , startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false).stroke()
+        }
         
-        let awe13 = UIBezierPath(arcCenter: CGPoint(x: 324, y:300), radius: 270, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
-        awe13.stroke()
-        
-        
-        
-        let awe123567 = UIBezierPath(arcCenter: CGPoint(x: 124, y:300), radius: 130, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
-        awe123567.stroke()
-        
-        let awe134567 = UIBezierPath(arcCenter: CGPoint(x: 324, y:300), radius: 210, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
-        awe134567.stroke()
+        let answerPoints: [CGPoint] = [
+           
+//            CGPoint(x: 54, y: 300),
+//            CGPoint(x:158 , y: 176),
+//            CGPoint(x: 158, y: 426),
+//            CGPoint(x:170 , y: 171),
+//            CGPoint(x: 170, y: 430),
+//            CGPoint(x:185, y: 167),
+//            CGPoint(x: 185, y: 433),
+//            CGPoint(x: 205, y: 159),
+//            CGPoint(x: 205, y: 439),
+//            CGPoint(x: 222, y: 159),
+//            CGPoint(x: 222, y: 439),
+//            CGPoint(x: 239, y: 165),
+//            CGPoint(x: 239, y: 434),
+//
+//            CGPoint(x: 259, y: 165),
+//            CGPoint(x: 259, y: 434),
+//            CGPoint(x: 274, y: 163),
+//            CGPoint(x: 274, y: 434),
+//            CGPoint(x: 290, y: 176),
+//            CGPoint(x: 290, y: 430),
+//            new
+            CGPoint(x: 347, y: 205),
+            CGPoint(x: 345, y: 401),
+            CGPoint(x: 394, y: 300.0),
+            CGPoint(x: 90.5, y: 220.0),
+            CGPoint(x: 90.5, y: 384.5),
+            CGPoint(x: 140.5, y: 184.5),
+            CGPoint(x: 140.5, y: 421.0),
+            CGPoint(x: 190.5, y: 168.5),
+            CGPoint(x: 190.5, y: 437.0),
+            CGPoint(x: 240.5, y: 165.5),
+            CGPoint(x: 240.5, y: 439.0),
+            CGPoint(x: 293.5, y: 177.5),
+            CGPoint(x: 293.5, y: 429.0),
+            CGPoint(x: 54, y: 300),
+            CGPoint(x: 116.5, y: 199),
+            CGPoint(x: 165.0, y: 173.0),
+            CGPoint(x: 217.5, y: 163.5),
+            CGPoint(x: 266.5, y: 170.5),
+            CGPoint(x: 319, y: 187.5),
+            CGPoint(x: 370.5, y: 228.5),
+            CGPoint(x: 371.5, y: 373.5),
+            
+        ]
+        for point in answerPoints {
+            let ee14 = UIBezierPath(arcCenter: point , radius: 3, startAngle: 0 * CGFloat.pi, endAngle: 2 * CGFloat.pi, clockwise: false)
+            #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1).setFill()
+            
+            ee14.fill()
+        }
 
+        
     }
+    
 }
+//a = (bb - hh) / (2 * h)
