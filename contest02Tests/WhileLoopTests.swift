fa064b7eff543ee6054b86215aec687df3e89564
xiaobao <mariuspuzhouli@gmail.com>
2020-06-10 20:32:01 +0800

    we finished about "option01"

---
+++ contest02Tests/WhileLoopTests.swift
@@ -0,0 +1,33 @@
+//
+//  WhileLoopTests.swift
+//  contest02Tests
+//
+//  Created by xiaobao on 2020/6/6.
+//  Copyright © 2020 ethan. All rights reserved.
+//
+
+import XCTest
+
+class WhileLoopTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
